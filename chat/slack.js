ba72347a752617897e00f48a05281bae98a27ecd
Luke Kayaba <brainjackersvr@gmail.com>
2017-09-15 22:37:20 -0500

    Create slack.js

---
+++ chat/slack.js
@@ -0,0 +1,65 @@
+const config = {
+    user: {
+        nick: "SLACK NICK",
+        user: "SLACK NICK",
+        real: "HOST USERNAME",
+        pass: "IRC_KEY"
+    },
+    server: {
+        addr: "DOMAIN.irc.slack.com",
+        port: 6667
+    }
+};
+
+var message, sender, lastChannel;
+
+var net = require("net");
+
+var ircSock = new net.Socket();
+
+ircSock.on("data", (data) => {
+    if (data.substr(0, 6) === "PING :") {
+        ircSock.write("PONG :" + data.split(":")[1] + "\r\n");
+        return;
+    }
+    
+    if (data.includes(" PRIVMSG #")) {
+        if (data.split("PRIVMSG #")[1].split(":")[1].substr(0, 1) === "!") {
+            message = data.split("PRIVMSG #")[1].split(":")[1];
+            sender = data.substr(1, data.indexOf("!") - 1);
+            lastChannel = data.split("PRIVMSG #")[1].split(" :")[0];
+            module.exports.scheduler.emit("msg");
+        }
+    }
+});
+
+ircSock.on("connect", () => {
+    setTimeout(() => {
+        ircSock.write("PASS " + config.user.pass + "\r\n");
+        ircSock.write("NICK " + config.user.nick + "\r\n");
+        ircSock.write("USER " + config.user.user + " 8 * :" + config.user.real + "\r\n");
+    }, 1000);
+});
+
+ircSock.setEncoding("utf-8");
+ircSock.setNoDelay();
+
+module.exports = {
+    getMsg: () => {
+        return message;
+    },
+
+    getSender: () => {
+        return sender;
+    },
+    
+    connect: () => {
+        ircSock.connect(config.server.port, config.server.addr);
+    },
+
+    send: (msg) => {
+        ircSock.write("PRIVMSG #" + lastChannel + " :" + msg + "\r\n");
+    },
+
+    scheduler: new (require("events"))()
+};
